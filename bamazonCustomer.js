738389d6a0fb9cb91b8811783a790c2f45acaf8a
asalinas9 <salinas99@live.com>
2020-04-19 15:24:29 -0500

    removed password

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,8 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "SLOsali9",
+    //removed password
+    password: "",
     database: "bamazon"
 });
 
